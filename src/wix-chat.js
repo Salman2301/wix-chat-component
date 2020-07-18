210f4923a26d071507df878f9cb3d50988f720e9
salman2301 <salmanhammed77@gmail.com>
2020-07-18 13:26:48 +0530

    added a test

--- src/wix-chat.js
+++ src/wix-chat.js
@@ -6,202 +6,7 @@ import("./timeago.js");
 const defaultImage ="https://static.wixstatic.com/media/46e3aa_0fe6d740591a4f589692d326953b7bde~mv2.png";
 
 const template = document.createElement("template");
-template.innerHTML = `
-<div class="container">
-  <!-- LODE MORE SECTION -->
-  <div class="loadmore">
-    <button type="button" class="btn-loadmore" id="btnLoadMore" >Load more</button>
-  </div>
-
-  <!-- MESSAGE BODY SECTION -->
-  <div id="msg-box"></div>
-
-  <!-- TYPING SECTION -->
-  <div class="typing">
-    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-    <p>Typing...</p>
-  </div>
-
-</div>
-
-<style>
-  :host{
-    --brand-color: #5cb85c;
-    --brand-color-dark: #3c783c;
-    --brand-color-blue: #1a73e8;
-    --brand-color-grey: #ebe7e7;
-    --brand-gradient-grey: linear-gradient(10deg, aliceblue, white);
-    --brand-gradient-blue: linear-gradient(10deg, #348bff, #ff4aff);
-    --brand-color-hover: #e4eef8;
-  }
-      
-  .container {
-    height: 400px;
-    background-color: rgba(0,0,0,0);
-    overflow: overlay;
-    display: block;
-  }
-  .message {
-    width: 90%;
-    display: inline-flex;
-    margin: 0px 10px;
-    bottom: 0;
-  }
-  .message:hover .timeAgo {
-    /* visibility: visible; */
-  }
-  .message-head {
-    text-align: center;
-    width: 15%;
-    margin: auto;
-  }
-  .message-body {
-    padding: 0px 10px 5px 10px;
-    width: 85%;
-    min-height: 60px;
-    /* background: var(--brand-color-grey); */
-    background-image: var(--brand-gradient-grey);
-    margin: 10px 0px;
-    border: 1px solid #ccc;
-    border-radius: 15px;
-  }
-  .username {
-    margin: 0px;
-    font-weight: bold;
-    font-size: 17px;
-    word-break: break-all;
-  }
-  .timeAgo {
-    color: #bcbcbc;
-    margin: 4px 0 10px 0;
-    /* visibility: hidden; */
-  }
-  .alter > .message-body {
-    /* background-color: var(--brand-color-blue); */
-    background-image: var(--brand-gradient-blue);
-    color: white;
-    direction: ltr;
-  }
-  .alter {
-    direction: rtl;
-    float: right;
-  }
-  .text-msg{
-    margin: 0 4px 4px 4px;
-    padding: 4;
-  }
-  .avatar {
-    border-radius: 50%;
-    /* position: relative; */
-    width: 35px;
-    height: 35px;
-  }
-  p {
-    font-size: 14px;
-    font-family: arial, sans-serif;
-  }
-
-  /* loading animation*/
-  .typing {
-    display: none;
-  }
-  .typing>p {
-    line-height: 3.5;
-  }
-  .lds-ellipsis {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-  }
-  .lds-ellipsis div {
-    position: absolute;
-    top: 33px;
-    width: 13px;
-    height: 13px;
-    border-radius: 50%;
-    background: #eee;
-    animation-timing-function: cubic-bezier(0, 1, 1, 0);
-  }
-  .lds-ellipsis div:nth-child(1) {
-    left: 8px;
-    animation: lds-ellipsis1 0.6s infinite;
-  }
-  .lds-ellipsis div:nth-child(2) {
-    left: 8px;
-    animation: lds-ellipsis2 0.6s infinite;
-  }
-  .lds-ellipsis div:nth-child(3) {
-    left: 32px;
-    animation: lds-ellipsis2 0.6s infinite;
-  }
-  .lds-ellipsis div:nth-child(4) {
-    left: 56px;
-    animation: lds-ellipsis3 0.6s infinite;
-  }
-  @keyframes lds-ellipsis1 {
-    0% {
-      transform: scale(0);
-    }
-    100% {
-      transform: scale(1);
-    }
-  }
-  @keyframes lds-ellipsis3 {
-    0% {
-      transform: scale(1);
-    }
-    100% {
-      transform: scale(0);
-    }
-  }
-  @keyframes lds-ellipsis2 {
-    0% {
-      transform: translate(0, 0);
-    }
-    100% {
-      transform: translate(24px, 0);
-    }
-  }
-  /* load more css */
-  .loadmore {
-      text-align: center;
-      display: none;
-  }
-  .btn-loadmore {
-    margin: .2em .1em;
-    font-family: arial, sans-serif;
-    /* font-weight: 700; */
-    font-size: 1em;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    border: none;
-    border: 1px solid black;
-    border-radius: 3px;
-    padding: 5px 10px;
-    /* background: var(--brand-color); */
-    background-image: var(--brand-gradient-grey);
-    color: black;
-  }
-  .btn-loadmore:hover {
-    background: #e4eef8;
-  }
-</style>
-`;
-
-const getMsgItem = data => `
-  <div class="message ${data.isOwner ? "alter" : ""}">
-    <div class="message-head">
-      <img class="avatar" src=${data.user.image} alt="Avatar">
-      <p class=username>${data.isOwner ? "You" : data.user.name}</p>
-    </div>
-
-    <div class="message-body">
-      <p class="timeAgo" datetime=${data.date}>${data.timeAgoStr}</p>
-      <p class="text-msg">${data.msg}</p>
-    </div>
-  </div>
-`;
+template.innerHTML = mainHTML();
 
 // Shadow dom
 class MessagesComponent extends HTMLElement {
@@ -218,6 +23,22 @@ class MessagesComponent extends HTMLElement {
     this.$container = this._shadowRoot.getElementById("msg-box");
     this.$typing = this._shadowRoot.querySelector(".typing");
     this.$btnLoadMore = this._shadowRoot.querySelector(".loadmore");
+
+    // SET GLOBAL CSS
+    let styleSheet = document.createElement("style");
+    // styleSheet.append(global_css);
+    
+    styleSheet.type = 'text/css';
+    if (styleSheet.styleSheet){
+      // This is required for IE8 and below.
+      styleSheet.styleSheet.cssText = global_css;
+    } else {
+      styleSheet.appendChild(document.createTextNode(get_global_css()));
+    }
+
+    this._shadowRoot.appendChild(styleSheet);
+    
+    // console.log({styleSheet});
 
   }
   
@@ -410,3 +231,211 @@ class MessagesComponent extends HTMLElement {
 }
 
 window.customElements.define("msg-component", MessagesComponent);
+
+
+
+// helper functions
+function mainHTML() {
+  return `
+  <div class="container">
+    <!-- LODE MORE SECTION -->
+    <div class="loadmore">
+      <button type="button" class="btn-loadmore" id="btnLoadMore" >Load more</button>
+    </div>
+
+    <!-- MESSAGE BODY SECTION -->
+    <div id="msg-box"></div>
+
+    <!-- TYPING SECTION -->
+    <div class="typing">
+      <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+      <p>Typing...</p>
+    </div>
+
+  </div>
+`
+}
+
+function getMsgItem(data){
+  return `
+  <div class="message ${data.isOwner ? "alter" : ""}">
+    <div class="message-head">
+      <img class="avatar" src=${data.user.image} alt="Avatar">
+      <p class=username>${data.isOwner ? "You" : data.user.name}</p>
+    </div>
+
+    <div class="message-body">
+      <p class="timeAgo" datetime=${data.date}>${data.timeAgoStr}</p>
+      <p class="text-msg">${data.msg}</p>
+    </div>
+  </div>
+`;
+}
+
+function get_global_css() {
+  return `
+  :host{
+    --brand-color: #5cb85c;
+    --brand-color-dark: #3c783c;
+    --brand-color-blue: #1a73e8;
+    --brand-color-grey: #ebe7e7;
+    --brand-gradient-grey: linear-gradient(10deg, aliceblue, white);
+    --brand-gradient-blue: linear-gradient(10deg, #348bff, #ff4aff);
+    --brand-color-hover: #e4eef8;
+  }
+      
+  .container {
+    height: 400px;
+    background-color: rgba(0,0,0,0);
+    overflow: overlay;
+    display: block;
+  }
+  .message {
+    width: 90%;
+    display: inline-flex;
+    margin: 0px 10px;
+    bottom: 0;
+  }
+  .message:hover .timeAgo {
+    /* visibility: visible; */
+  }
+  .message-head {
+    text-align: center;
+    width: 15%;
+    margin: auto;
+  }
+  .message-body {
+    padding: 0px 10px 5px 10px;
+    width: 85%;
+    min-height: 60px;
+    /* background: var(--brand-color-grey); */
+    background-image: var(--brand-gradient-grey);
+    margin: 10px 0px;
+    border: 1px solid #ccc;
+    border-radius: 15px;
+  }
+  .username {
+    margin: 0px;
+    font-weight: bold;
+    font-size: 17px;
+    word-break: break-all;
+  }
+  .timeAgo {
+    color: #bcbcbc;
+    margin: 4px 0 10px 0;
+    /* visibility: hidden; */
+  }
+  .alter > .message-body {
+    /* background-color: var(--brand-color-blue); */
+    background-image: var(--brand-gradient-blue);
+    color: white;
+    direction: ltr;
+  }
+  .alter {
+    direction: rtl;
+    float: right;
+  }
+  .text-msg{
+    margin: 0 4px 4px 4px;
+    padding: 4;
+  }
+  .avatar {
+    border-radius: 50%;
+    /* position: relative; */
+    width: 35px;
+    height: 35px;
+  }
+  p {
+    font-size: 14px;
+    font-family: arial, sans-serif;
+  }
+
+  /* loading animation*/
+  .typing {
+    display: none;
+  }
+  .typing>p {
+    line-height: 3.5;
+  }
+  .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    top: 33px;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: #eee;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 8px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 8px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 32px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 56px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(24px, 0);
+    }
+  }
+  /* load more css */
+  .loadmore {
+      text-align: center;
+      display: none;
+    
+  }
+  .btn-loadmore {
+    margin: .2em .1em;
+    font-family: arial, sans-serif;
+    /* font-weight: 700; */
+    font-size: 1em;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    border: none;
+    border: 1px solid black;
+    border-radius: 3px;
+    padding: 5px 10px;
+    /* background: var(--brand-color); */
+    background-image: var(--brand-gradient-grey);
+    color: black;
+    cursor: pointer;
+  }
+  .btn-loadmore:hover {
+    background: #e4eef8;
+  }
+`;
+}
